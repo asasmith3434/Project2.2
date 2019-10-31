ea84f5b1f43c25283ae0c5dcbacd176a16735774
Asa C. Smith <asa.smith@centre.edu>
2019-10-30 21:45:35 -0400

    add

--- index.js
+++ index.js
@@ -20,21 +20,26 @@ var displayNames= function(PlanetsData) // for the All Planets Click
     d3.select(".names")
     .append("h2")
     .text("All Planets")
-    .on("click", function(d) {printNames(PlanetsData)})
+    .on("click", function(d) {
+         d3.selectAll("ul *")
+        .remove()
+        printNames(PlanetsData)}  )
 }
 
 var printNames = function(PlanetsData)
 {
-    console.log(PlanetsData)
-    d3.selectAll("ul *")
-    .remove()
-    d3.select("ul")
+   
+   
+    d3.select(".names")
+    .append("ul")
     .selectAll("li")
     .data(PlanetsData)
     .enter()
     .append("li")
-    .text(function(d) { return d.name })
-    .on("click", function(d) { printfirstData(d) });
+    .text(function(d) { return d.name } ) 
+    .on("click", function(d) { 
+        printfirstData(d) } );
+    
 }
 
 
@@ -73,42 +78,88 @@ var displayHabitable= function(PlanetsData) // for the Habital click
     d3.select(".names2")
     .append("h2")
     .text("Habitable")
-    .on("click", function(d) {printNames2(PlanetsData)})
+    .on("click", function(d) {
+        d3.selectAll("ul *")
+        .remove()
+        printNames2(PlanetsData)})
 }
 var printNames2= function(PlanetsData)
 {
     console.log(PlanetsData)
-    d3.selectAll("ul *")
+   // d3.select("#names2")
+    //.remove()
+   var Habitable= filterHab(PlanetsData)
+    d3.select(".names2")
+    .append("ul")
+    .selectAll("li")
     .remove()
-    d3.select("ul")
-   .selectAll("li")
-    .data(PlanetsData)
+    .data(Habitable)
     .enter()
     .append("li")
     .text(function(d) { return d.name })
-    .on("click",   printData.filter(function(printData){ if (PlanetData.population>0) {return printData;} });
+   .on("click",  function(d) {printData (d)})
+       
+        
+       
     
+        
+        
+        
+        
 }
+// must erase all the data and then replot the filtered data. Because the filter only reorganizes and does not display the data back into html. Use .remove to erase the data and then call the funciton(data) again to replot.
 
-
-var printData = function(PlanetData)
+var printData = function(Planet2Data)
 {
     d3.select(".data *").remove("ul");
     d3.select(".data").append("div").attr("class", "info");
     d3.select(".info").append("ul").attr("class", "infoList");
-    d3.select(".infoList").append("li").text(PlanetData.name);
-    d3.select(".infoList").append("li").text("Terrain:"+PlanetData.terrain);
-    d3.select(".infoList").append("li").text("Climate:"+PlanetData.climate)
-    d3.select(".infoList").append("li").text("Population:"+PlanetData.population)
-    d3.select(".infoList").append("li").text("Orbital Period:"+PlanetData.orbital_period)
-    d3.select(".infoList").append("li").text("Rotational Period:"+PlanetData.rotation_period)
+    d3.select(".infoList").append("li").text(Planet2Data.name);
+    d3.select(".infoList").append("li").text("Terrain:"+Planet2Data.terrain);
+    d3.select(".infoList").append("li").text("Climate:"+Planet2Data.climate)
+    d3.select(".infoList").append("li").text("Population:"+Planet2Data.population)
+    d3.select(".infoList").append("li").text("Orbital Period:"+Planet2Data.orbital_period)
+    d3.select(".infoList").append("li").text("Rotational Period:"+Planet2Data.rotation_period)
 
 
 }
 
 
+var filterHab= function(PlanetsData) 
+        {
+       return PlanetsData.filter(function(planet) 
+        {
+            if (planet.population>0) 
+                    {
+                        return true;
+                    }
+            else{ return  false;}
+         })
+        
+        
+    }
+    
+  
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ // or = || population== 0 || population== unknown   
 
 
 
@@ -119,21 +170,25 @@ var displayInhabitable= function(PlanetsData) // for the All Planets Click
     d3.select(".names3")
     .append("h2")
     .text("Inhabitable")
-    .on("click", function(d) {printNames(PlanetsData)})
+    .on("click", function(d) {
+        d3.selectAll("ul *")
+        .remove()
+        printNames3(PlanetsData)})
 }
 
 var printNames3 = function(PlanetsData)
 {
-    console.log(PlanetsData)
-    d3.selectAll("ul *")
-    .remove()
-    d3.select("ul")
+    
+    var Inhabitable= filterHab2(PlanetsData)
+    d3.select(".names3")
+    .append("ul")
     .selectAll("li")
-    .data(PlanetsData)
+    .data(Inhabitable)
     .enter()
     .append("li")
     .text(function(d) { return d.name })
-    .on("click", function(d) { printsecondData(d) });
+    .on("click", function(d) { 
+        printsecondData(d) });
 }
 
 
@@ -151,6 +206,32 @@ var printsecondData = function(PlanetData)
 
 
 }
+
+
+var filterHab2= function(PlanetsData) 
+        {
+       return PlanetsData.filter(function(planetInhabitable) 
+        {
+            if (planetInhabitable.population==0 || planetInhabitable.population=="unknown") 
+                    {
+                        return true;
+                    }
+            else{ return  false;}
+         })
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
